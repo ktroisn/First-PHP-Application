4e464aa96fe250817782f2d34b6c7a73bfccdf79
ktroisn <brouillonquentin@gmail.com>
2023-05-12 13:34:13 +0200

    add comments and code fore button delete
    I added several buttons in recap.php, I added the functionality to make these buttons work, with some conditions

--- appli/traitement.php
+++ appli/traitement.php
@@ -1,14 +1,18 @@
 <?php 
+    // Démarre une session PHP
     session_start();
     
+    // Si le formulaire d'ajout de produit a été soumis
     if(isset($_POST['submit'])){
-        
-        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+        // Récupère le nom, le prix et la quantité saisis dans le formulaire
+        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
         $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
         
+        // Vérifie que les données saisies sont valides
         if($name && $price && $qtt){
             
+            // Crée un tableau associatif contenant les informations du produit
             $product = [
                 "name" => $name,
                 "price" => $price,
@@ -16,12 +20,59 @@
                 "total" => $price*$qtt 
             ];
 
+            // Ajoute le produit à la liste des produits enregistrés en session
             $_SESSION['products'][] = $product;
-
+            // Stocke un message de confirmation dans la session
+            $_SESSION['check'] = "<p style='background: rgb(33, 244, 178); padding: 10px 10px 9px 45px; border-radius: 20px; color: white;'>Produit ajouté avec succès.</p>";       
+        } else {
+            // Stocke un message d'erreur dans la session si les données saisies ne sont pas valides
+            $_SESSION['check'] = "<p style='background: rgb(239, 138, 138); padding: 10px 10px 9px 45px; border-radius: 20px; color: white;'>Erreur. Vérifiez votre saisie.</p>";
         }
 
     }
 
+    // Si le formulaire de suppression de tous les produits a été soumis, détruit la session
+    if(isset($_POST['deleteall'])){
+        session_destroy();
+    }
+    
+    // Redirige l'utilisateur vers la page d'accueil
     header("Location:index.php");
+    
+    // Parcourt la liste des produits enregistrés en session
+    foreach ($_SESSION['products'] as $index => $value){
+        // Si le formulaire de suppression du produit en cours a été soumis, supprime le produit de la session
+        if(isset($_POST[$index])){
+            unset($_SESSION['products'][$index]); 
+            // Redirige l'utilisateur vers la page de récapitulatif des produits
+            header("Location:recap.php"); 
+        }
+    }
 
+    // Parcourt la liste des produits enregistrés en session
+    foreach ($_SESSION['products'] as $index => $product) {
+        // Si le formulaire d'ajout de quantité du produit en cours a été soumis, incrémente la quantité et met à jour le total
+        if (isset($_POST[$index . 'add'])) { 
+            // Incrémente la quantité du produit en cours
+            $_SESSION['products'][$index]['qtt'] += 1;
+            // Met à jour le total du produit en fonction de sa quantité mise à jour
+            $_SESSION['products'][$index]['total'] = $_SESSION['products'][$index]['price'] * $_SESSION['products'][$index]['qtt'];
+            // Redirige l'utilisateur vers la page de récapitulatif des produits
+            header("Location: Recap.php");
+            
+        } elseif (isset($_POST[$index . 'remove'])) {
+            // Si le formulaire de suppression de quantité du produit en cours a été soumis
+            if ($_SESSION['products'][$index]['qtt'] > 1) {
+                // Décrémente la quantité du produit en cours si elle est supérieure à 1
+                $_SESSION['products'][$index]['qtt'] -= 1;
+                // Met à jour le total du produit en fonction de sa quantité mise à jour
+                $_SESSION['products'][$index]['total'] = $_SESSION['products'][$index]['price'] * $_SESSION['products'][$index]['qtt'];
+            }
+            // Redirige l'utilisateur vers la page de récapitulatif des produits
+            header("Location: Recap.php");
+        
+        }
+    }
+
+    
 ?>
